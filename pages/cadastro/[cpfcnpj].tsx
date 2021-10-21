d5c8d929b12eb49f94f518542aa809a0cbce5935
Bruno <bhscorreia@gmail.com>
2021-10-21 18:08:52 -0300

    202110211808

--- pages/cadastro/[cpfcnpj].tsx
+++ pages/cadastro/[cpfcnpj].tsx
@@ -4,7 +4,7 @@ import Layout from "../../components/Layout";
 
 function CadastroPage() {
     const router = useRouter();
-    const cpfcnpj: string = router.query.cpfcnpj.toString();
+    const cpfcnpj: string = router.query.cpfcnpj?.toString();
 
     preencherCadastro(cpfcnpj);
 
