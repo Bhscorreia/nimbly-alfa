911f5da31e5cf2680fba3e4a999d07cbcc0c57a4
Bruno <bhscorreia@gmail.com>
2021-10-21 18:01:37 -0300

    pequeno ajuste no layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -23,18 +23,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
           <span className="navbar-toggler-icon"></span>
         </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-            <Link href="/">
-              <a className="nav-link active" aria-current="page">Home</a>
-            </Link>
-            </li>
-            <li className="nav-item">
-            <Link href="/about">
-              <a className="nav-link">Sobre nós</a>
-            </Link>
-            </li>
-          </ul>
         </div>
       </div>
     </nav>
