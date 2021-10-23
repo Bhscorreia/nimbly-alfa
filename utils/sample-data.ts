85866318802209911b4fb3c60c09ddc3b56ae760
Bruno <bhscorreia@gmail.com>
2021-10-23 12:27:27 -0300

    202110231228

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -8,8 +8,8 @@ export const sampleUserData: Pessoa[] = [
     name: "Mario carlos",
     documentType: tipoDocumento.CPF,
     cpfcnpj: "19238788758",
-    brandName: 'Organização A',
-    companyCnpj: '21128159000166',
+    brandName: "Organização A",
+    companyCnpj: "21128159000166",
     personalId: "578-psd-71md6971kjh-2d414",
     requestDateTime: "2021-01-28T08:30:00Z",
     updateDateTime: "2021-01-28T08:30:00Z"
